60fb7f82215a756911384d85cd88d2479fa2ef4c
mossamor97 <mohamedadel153707@gmail.com>
2018-08-16 07:31:18 +0200

    done

--- pages/models.py
+++ pages/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Urls(models.Model):
     long = models.URLField(unique=True)
-    short = models.CharField(max_length=200, unique=True)
+    short = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.short
--- pages/views.py
+++ pages/views.py
@@ -3,25 +3,26 @@ from . import models
 from .models import Urls
 from django.http import HttpResponseRedirect ,HttpResponse, HttpResponseNotFound
 import string , random
+
+
+
 def encode(request):
     if request.method == 'POST':
-        exist = Urls.objects.filter(long = request.POST.get('name'))
+        exist = Urls.objects.filter(long = request.POST.get('long_url_name'))
         if len(exist)<=0:
-            strin = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345678901234567890123456789'
+            strin = '0123456789abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
             my_str =''
             for c in range(15):
                 my_str += random.choice(strin)
-            my_url = Urls(long = request.POST.get('name') , short = my_str)
+            my_url = Urls(long = request.POST.get('long_url_name') , short = my_str)
             my_url.save()
-            return render(request,'home.html',{'name':my_str})
+            return render(request,'home.html',{'short_url_name':my_str})
         else:
-            return render(request,'home.html',{'name':Urls.objects.get(long = request.POST.get('name'))})
+            return render(request,'home.html',{'short_url_name':Urls.objects.get(long = request.POST.get('long_url_name'))})
 
     elif request.method == "GET":
-        return render(request,'home.html',{'name':""})
+        return render(request,'home.html',{'short_url_name':""})
             
-
-
 
 
 
@@ -32,8 +33,3 @@ def decode(request, short):
         return HttpResponseRedirect(loong)
     except Urls.DoesNotExist:
         return HttpResponseNotFound()
-
-
-
-
-# Create your views here.
